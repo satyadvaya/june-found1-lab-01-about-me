364f8da9fe28a346e4cf2f54adf80c597e80fd4a
satyadvaya <satyadvaya@gmail.com>
2021-06-29 10:56:46 -0700

    initial commit

--- app.js
+++ app.js
@@ -6,3 +6,14 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  //  get the button element
+  //  add "click" event listener to the button
+  //    confirm the user wants to start the quiz
+  //    if they do -- prompt for at least 3 questions and store the anwer
+  //    for each answer, check if the user entered a correct value
+  //    at the end of the quiz, update the results section with the number of correct answers
+
+  const quizButton = document.getElementById("click-me");
+  const results = document.getElementById("results");
